fef9b66a1fcdd72ea1ad659a2b11c4f912b80d43
erinantonia <145416233+erinantonia@users.noreply.github.com>
2024-06-10 13:03:53 +1200

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <footer>
+        <p>
+          Coded by{" "}
+          <a href="https://github.com/erinantonia" target="_blank">
+            Erin Allen
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/erinantonia/react-weather-project"
+            target="_blank"
+          >
+            {" "}
+            Github.
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
